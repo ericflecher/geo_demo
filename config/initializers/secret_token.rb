d3dbec0f02aa6b659191cff3d9829ba58abe8000
David Justice <david@David-Justices-MacBook-Pro.local>
2011-01-23 17:29:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoDemo::Application.config.secret_token = 'a871eebde6a2f52be89cff7b9823055db89a17ae2830184177e1b37bbcdda6943b40a016d3ef87d9662fd37d0e14664969089bacb4e37cb1704b11a583213697'
